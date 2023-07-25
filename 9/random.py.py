3b96cd1882facf4fd4fbef2f8e2c28241ebed962
Ben <137895968+benyaminkhaleh@users.noreply.github.com>
2023-07-25 11:14:22 +0330

    Delete random.py.py
    Signed-off-by: Ben <137895968+benyaminkhaleh@users.noreply.github.com>

--- 9/random.py.py
+++
@@ -1,8 +0,0 @@
-import random
-my_list=[]
-number = 0
-tole=int(input('Tole list ra mosshakhas konid: '))
-for i in range(tole):
-    number= random.randint(0,100)
-    my_list.append(number)
-print(my_list)
