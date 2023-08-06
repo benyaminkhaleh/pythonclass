f6bcb31d9407b2043578430d063997dbbeba9061
Ben <137895968+benyaminkhaleh@users.noreply.github.com>
2023-08-06 11:20:15 +0330

    14
    Signed-off-by: Ben <137895968+benyaminkhaleh@users.noreply.github.com>

---
+++ 14/opp.py
@@ -0,0 +1,29 @@
+class Teacher:
+    name = None
+    age = None
+    Work_Experience = None
+    ethics = None
+    def teach() : pass
+    def ask_question() : pass
+    def write() : pass
+    def Conversation() : pass
+    def question() : pass
+    def answer() : pass
+class Student:
+    name = None
+    age = None
+    Grade = None
+    score = None
+    def learn() : pass
+    def write() : pass
+    def listen() : pass
+    def question() : pass
+    def answer() : pass
+class course :
+    member = None
+    name = None
+    target = None
+    title = None
+    time = None
+    def Gathering_people(): pass
+    def Transforming_beginner_into_professional(): pass
